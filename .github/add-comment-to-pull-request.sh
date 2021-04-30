0820ae2b7228bb4987a51f9a9ab09f13f153a08e
Derek Smart <derek@grindaga.com>
2021-04-30 12:52:20 -0400

    test
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- .github/add-comment-to-pull-request.sh
+++ .github/add-comment-to-pull-request.sh
@@ -13,14 +13,14 @@ if [[ -z "$GITHUB_ISSUE_URL" ]]; then
   exit 1
 fi
 
-if [[ -z "$GITHUB_LABEL" ]]; then
-  echo "Set the GITHUB_LABEL env variable."
+if [[ -z "$GITHUB_COMMENT" ]]; then
+  echo "Set the GITHUB_COMMENT env variable."
   exit 1
 fi
 
-data='["'${GITHUB_LABEL}'"]'
+data='{"body":"'${GITHUB_COMMENT}'"}'
 
 #Create Label on Pull Request
 curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
   --data "$data" \
-  "$GITHUB_ISSUE_URL/labels"
+  "$GITHUB_ISSUE_URL/comments"
